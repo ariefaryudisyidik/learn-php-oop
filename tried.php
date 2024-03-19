013013b1d127024e307bee669a6200dc07628c09
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-03-19 07:05:01 +0700

    feat: Add Class & Object

---
+++ tried.php
@@ -0,0 +1,16 @@
+<?php
+
+class Tried
+{
+
+}
+
+// Create object instance of class
+
+$triedOne = new Tried();
+$triedTwo = new Tried();
+$triedThree = new Tried();
+
+var_dump($triedOne);
+var_dump($triedTwo);
+var_dump($triedThree);
